3f0ecd2a91268a245680d42fc459f6bd45ab3689
Neptr1209 <160880389+Neptr1209@users.noreply.github.com>
2026-04-09 12:47:48 +0700

    Update app.dart

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,25 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:phsar_kaksekor_app/core/themes/app_theme.dart';
-import 'routes.dart';
 import 'package:phsar_kaksekor_app/providers/auth_provider.dart';
 import 'package:phsar_kaksekor_app/providers/cart_provider.dart';
 import 'package:phsar_kaksekor_app/providers/product_provider.dart';
 import 'package:phsar_kaksekor_app/providers/order_provider.dart';
 import 'package:phsar_kaksekor_app/providers/user_provider.dart';
+import 'package:phsar_kaksekor_app/data/mock_data.dart';
+import 'routes.dart';
 
 class PsarKaksekorApp extends StatelessWidget {
   const PsarKaksekorApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // Create providers
+    final authProvider    = AuthProvider();
+    final cartProvider    = CartProvider();
+    final productProvider = ProductProvider();
+    final orderProvider   = OrderProvider();
+    final userProvider    = UserProvider();
+
+    // Seed dummy data
+    seedMockData(
+      productProvider: productProvider,
+      orderProvider:   orderProvider,
+      authProvider:    authProvider,
+      userProvider:    userProvider,
+    );
+
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => AuthProvider()),
-        ChangeNotifierProvider(create: (_) => CartProvider()),
-        ChangeNotifierProvider(create: (_) => ProductProvider()),
-        ChangeNotifierProvider(create: (_) => OrderProvider()),
-        ChangeNotifierProvider(create: (_) => UserProvider()),
+        ChangeNotifierProvider.value(value: authProvider),
+        ChangeNotifierProvider.value(value: cartProvider),
+        ChangeNotifierProvider.value(value: productProvider),
+        ChangeNotifierProvider.value(value: orderProvider),
+        ChangeNotifierProvider.value(value: userProvider),
       ],
       child: MaterialApp(
         title: 'Psar Kaksekor',
